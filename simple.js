71a4f76de1f310c5b52e1ae563ed6a48f03a477b
Jason Frame <jason@onehackoranother.com>
2013-05-14 15:20:49 +0100

    Token definitions

--- simple.js
+++ simple.js
@@ -1,1 +1,49 @@
 simple = {};
+
+;(function(simple) {
+  
+  var nextToken = 1;
+  
+  simple.TOKENS = {};
+  
+  [ 'SUB',
+    'ADD',
+    'MUL',
+    'DIV',
+    'BANG',
+    'LT',
+    'GT',
+    'LE',
+    'GE',
+    'EQ',
+    'NEQ',
+    'TILDE',
+    'TRUE',
+    'FALSE',
+    'INTEGER',
+    'FLOAT',
+    'STRING',
+    'TRACE',
+    'IDENT',
+    'IF',
+    'ELSE',
+    'WHILE',
+    'DEF',
+    'L_BRACE',
+    'R_BRACE',
+    'L_BRACKET',
+    'R_BRACKET',
+    'L_PAREN',
+    'R_PAREN',
+    'SEMICOLON',
+    'NEWLINE',
+    'IDENT',
+    'COMMA',
+    'RETURN',
+    'ASSIGN',
+    'EOF'
+  ].forEach(function(tok) {
+    simple.TOKENS[tok] = nextToken++;
+  });
+  
+})(simple);
