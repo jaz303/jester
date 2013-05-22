131ba336d3a7a366ac2b8a5d1c579bec10a2d5e8
Jason Frame <jason@onehackoranother.com>
2013-05-23 00:32:38 +0100

    Parser stores line information in AST

--- parser.js
+++ parser.js
@@ -117,9 +117,11 @@
     }
     
     function parseFunctionDefinition() {
+      var line = lexer.line();
+      
       accept(T.DEF);
       
-      var node = { type: 'def' };
+      var node = { type: 'def', line: line };
       
       if (at(T.IDENT)) {
         node.name = text();
@@ -143,8 +145,10 @@
     }
     
     function parseIfStatement() {
+      var line = lexer.line();
+      
       accept(T.IF);
-      var node = { type: 'if', clauses: [] };
+      var node = { type: 'if', line: line, clauses: [] };
       
       var cond = parseExpression();
       skipNewlines();
@@ -178,8 +182,9 @@
     }
     
     function parseWhileStatement() {
+      var line = lexer.line();
       accept(T.WHILE);
-      var node = { type: 'while' };
+      var node = { type: 'while', line: line };
       node.condition = parseExpression();
       skipNewlines();
       node.body = parseStatementBlock();
@@ -187,8 +192,9 @@
     }
     
     function parseLoopStatement() {
+      var line = lexer.line();
       accept(T.LOOP);
-      var node = { type: 'loop' };
+      var node = { type: 'loop', line: line };
       skipNewlines();
       node.body = parseStatementBlock();
       return node;
@@ -205,9 +211,10 @@
     }
     
     function parseReturnStatement() {
+      var line = lexer.line();
       accept(T.RETURN);
       
-      var node = { type: 'return', returnValue: undefined };
+      var node = { type: 'return', line: line, returnValue: undefined };
       
       if (atBlockTerminator()) {
         /* do nothing */
@@ -273,11 +280,13 @@
     function parseAssignExpression() {
       var exp = parseLogicalOrExpression();
       if (at(T.ASSIGN)) {
+        var line = lexer.line();
         next();
-        var root = { type: 'assign', left: exp, right: parseLogicalOrExpression() }, curr = root;
+        var root = { type: 'assign', line: line, left: exp, right: parseLogicalOrExpression() }, curr = root;
         while (at(T.ASSIGN)) {
+          line = lexer.line();
           next();
-          curr.right = { type: 'assign', left: curr.right, right: parseLogicalOrExpression() };
+          curr.right = { type: 'assign', line: line, left: curr.right, right: parseLogicalOrExpression() };
           curr = curr.right;
         }
         return root;
@@ -289,8 +298,9 @@
     function parseLogicalOrExpression() {
       var exp = parseLogicalAndExpression();
       while (at(T.LOR)) {
+        var line = lexer.line();
         next();
-        exp = { type: TOKEN_OP[T.LOR], left: exp, right: parseLogicalAndExpression() };
+        exp = { type: TOKEN_OP[T.LOR], line: line, left: exp, right: parseLogicalAndExpression() };
       }
       return exp;
     }
@@ -298,8 +308,9 @@
     function parseLogicalAndExpression() {
       var exp = parseEqualityExpression();
       while (at(T.LAND)) {
+        var line = lexer.line();
         next();
-        exp = { type: TOKEN_OP[T.LAND], left: exp, right: parseEqualityExpression() };
+        exp = { type: TOKEN_OP[T.LAND], line: line, left: exp, right: parseEqualityExpression() };
       }
       return exp;
     }
@@ -307,9 +318,10 @@
     function parseEqualityExpression() {
       var exp = parseCmpExpression();
       while (at(T.EQ) || at(T.NEQ)) {
+        var line = lexer.line();
         var op = TOKEN_OP[curr];
         next();
-        exp = { type: op, left: exp, right: parseCmpExpression() };
+        exp = { type: op, line: line, left: exp, right: parseCmpExpression() };
       }
       return exp;
     }
@@ -317,9 +329,10 @@
     function parseCmpExpression() {
       var exp = parseAddSubExpression();
       while (at(T.LT) || at(T.GT) || at(T.LE) || at(T.GE)) {
+        var line = lexer.line();
         var op = TOKEN_OP[curr];
         next();
-        exp = { type: op, left: exp, right: parseAddSubExpression() };
+        exp = { type: op, line: line, left: exp, right: parseAddSubExpression() };
       }
       return exp;
     }
@@ -328,9 +341,10 @@
     function parseAddSubExpression() {
       var exp = parseMulDivExpression();
       while (at(T.ADD) || at(T.SUB)) {
+        var line = lexer.line();
         var op = TOKEN_OP[curr];
         next();
-        exp = { type: op, left: exp, right: parseMulDivExpression() };
+        exp = { type: op, line: line, left: exp, right: parseMulDivExpression() };
       }
       return exp;
     }
@@ -339,9 +353,10 @@
     function parseMulDivExpression() {
       var exp = parseUnary();
       while (at(T.MUL) || at(T.DIV)) {
+        var line = lexer.line();
         var op = TOKEN_OP[curr];
         next();
-        exp = { type: op, left: exp, right: parseUnary() };
+        exp = { type: op, line: line, left: exp, right: parseUnary() };
       }
       return exp;
     }
@@ -349,10 +364,12 @@
     // !foo, ~foo, -foo, +foo
     function parseUnary() {
       if (at(T.BANG) || at(T.TILDE) || at(T.SUB) || at(T.ADD)) {
-        var root = { type: TOKEN_OP[curr], exp: null }, curr = root;
+        var line = lexer.line();
+        var root = { type: TOKEN_OP[curr], line: line, exp: null }, curr = root;
         next();
         while (at(T.BANG) || at(T.TILDE) || at(T.SUB) || at(T.ADD)) {
-          curr.exp = { type: TOKEN_OP[curr], exp: null };
+          line = lexer.line();
+          curr.exp = { type: TOKEN_OP[curr], line: line, exp: null };
           curr = curr.exp;
           next();
         }
@@ -365,9 +382,11 @@
     
     // foo(1, 2, 3)
     // foo 1, 2, 3
+    // TODO: disallow nested no-paren calls?
     function parseCall() {
-      var atom = parseAtom(), args = null;
+      var atom = parseAtom(), line, args = null;
       if (at(T.L_PAREN)) {
+        line = lexer.line();
         next();
         if (at(T.R_PAREN)) {
           args = [];
@@ -375,17 +394,18 @@
           args = parseExpressionList();
         }
         accept(T.R_PAREN);
-        return { type: 'call', fn: atom, args: args };
+        return { type: 'call', line: line, fn: atom, args: args };
       } else if (atExpStart()) {
+        line = lexer.line();
         var args = parseExpressionList();
-        return { type: 'call', fn: atom, args: args };
+        return { type: 'call', line: line, fn: atom, args: args };
       } else {
         return atom;
       }
     }
     
     function parseAtom() {
-      var exp = null;
+      var exp = null, line = lexer.line();
       
       if (at(T.TRUE)) {
         exp = true;
@@ -406,10 +426,10 @@
         exp = text();
         next();
       } else if (at(T.TRACE)) {
-        exp = { type: 'trace' };
+        exp = { type: 'trace', line: line };
         next();
       } else if (at(T.IDENT)) {
-        exp = { type: 'ident', name: text() };
+        exp = { type: 'ident', line: line, name: text() };
         next();
       } else if (at(T.L_PAREN)) {
         next();
