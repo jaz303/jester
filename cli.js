eb69a5cb4073e899fa04d4a13cd36d5d90cf328d
Jason Frame <jason@onehackoranother.com>
2013-08-29 16:23:49 +0100

    minor bugs fixed, cli working, use strict everywhere

--- cli.js
+++ cli.js
@@ -1,3 +1,13 @@
-var jester = require('./');
+var jester  = require('./'),
+    fs      = require('fs');
 
-console.log("JESTER!");
+var args = process.argv.slice(2);
+
+fs.readFile(args[0], 'utf8', function(err, source) {
+
+    var context = jester.createContext();
+
+    context.start();
+    context.run(source, args[0]);
+
+});
