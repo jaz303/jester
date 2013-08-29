86750a11ae928c6aeb50fa8821f53aae3739d3cc
Jason Frame <jason@onehackoranother.com>
2013-08-29 16:35:10 +0100

    fix executable, duh

--- cli.js
+++ cli.js
@@ -1,9 +1,11 @@
+#!/usr/bin/env node
+
 var jester  = require('./'),
     fs      = require('fs');
 
-var args = process.argv.slice(2);
+var args = process.argv.slice(1);
 
-fs.readFile(args[0], 'utf8', function(err, source) {
+fs.readFile(args[1], 'utf8', function(err, source) {
 
     var context = jester.createContext();
 
