12ab5bb1b71ac04490d4acef722fa232b68c4602
Jason Frame <jason@onehackoranother.com>
2013-09-15 04:07:17 +0200

    changes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 module.exports = {
     createContext   : require('./lib/context').createContext,
-    createVM        : require('./lib/vm').createVM
+    createVM        : require('./lib/vm').createVM,
+    constants       : require('./lib/constants'),
+    opcodeMeta      : require('./lib/vm').opcodeMeta
 };
