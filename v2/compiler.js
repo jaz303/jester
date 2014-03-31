5e41634f3de78b26e84f97a88815422071a0bdc7
Jason Frame <jason@onehackoranother.com>
2014-03-31 23:53:22 +0100

    error reporting for compileModule() is always dispatched to callback

--- v2/compiler.js
+++ v2/compiler.js
@@ -1,43 +1,50 @@
 module.exports = Compiler;
 
-var Module 		= require('./internals/Module'),
-	Precompiler	= require('./Precompiler'),
-	A 			= require('./ast_nodes');
+var Module      = require('./internals/Module'),
+    Precompiler = require('./Precompiler'),
+    A           = require('./ast_nodes');
+
+function makeAstTypeMismatchError(actual, expected) {
+    return new Error("AST node type mismatch, got " + actual + ", expected " + expected);
+}
 
 function expectNodeType(ast, type) {
-	if (ast.type !== type) {
-		throw new Error("AST node type mismatch, got " + ast.type, "expected " + type);
-	}
+    if (ast.type !== type) {
+        throw makeAstTypeMismatchError(ast.type, type);
+    }
 }
 
 function Compiler(context) {
-	this._context = context;
+    this._context = context;
 }
 
 Compiler.prototype._compileModule = function(mod) {
-	console.log("compiling: ", mod);
+    console.log("compiling: ", mod);
 }
 
 Compiler.prototype.compileModule = function(mod, cb) {
 
-	expectNodeType(mod, A.MODULE);
+    if (mod.type !== A.MODULE) {
+        cb(makeAstTypeMismatchError(mod.type, A.MODULE));
+        return;
+    }
 
-	var self = this;
+    var self = this;
 
-	var pre = new Precompiler(this._context);
-	pre.precompile(mod, function(err, loadOrder) {
-		if (err) {
-			cb(err);
-		} else {
-			try {
-				for (var i = 0; i < loadOrder.length; ++i) {
-					self._compileModule(loadOrder[i]);
-				}
-				cb(null, loadOrder);
-			} catch (e) {
-				cb(err);
-			}
-		}
-	});
+    var pre = new Precompiler(this._context);
+    pre.precompile(mod, function(err, loadOrder) {
+        if (err) {
+            cb(err);
+        } else {
+            try {
+                for (var i = 0; i < loadOrder.length; ++i) {
+                    self._compileModule(loadOrder[i]);
+                }
+                cb(null, loadOrder);
+            } catch (e) {
+                cb(err);
+            }
+        }
+    });
 
 }
