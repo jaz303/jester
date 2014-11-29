30cb6d5ceb5a53dd31d20f26bfdb598fbad1319d
Jason Frame <jason@onehackoranother.com>
2014-11-29 17:00:01 +0000

    start generating AST

--- v3/ast.js
+++ v3/ast.js
@@ -1,41 +1,76 @@
+var _n = 1; function iota() { return _n++; }
+
+var T_MODULE				= exports.T_MODULE 					= iota();
+var T_ARRAY_LITERAL			= exports.T_ARRAY_LITERAL			= iota();
+var T_DICTIONARY_LITERAL 	= exports.T_DICTIONARY_LITERAL 		= iota();
+var T_GLOBAL_OBJECT 		= exports.T_GLOBAL_OBJECT 			= iota();
+var T_GLOBAL_IDENT 			= exports.T_GLOBAL_IDENT 			= iota();
+var T_LAMBDA 				= exports.T_LAMBDA 					= iota();
+var T_IDENT 				= exports.T_IDENT 					= iota();
+var T_WHILE_STATEMENT 		= exports.T_WHILE_STATEMENT 		= iota();
+var T_LOOP_WHILE_STATEMENT 	= exports.T_LOOP_WHILE_STATEMENT 	= iota();
+var T_LOOP_STATEMENT 		= exports.T_LOOP_STATEMENT 			= iota();
+var T_FOR_STATEMENT 		= exports.T_FOR_STATEMENT 			= iota();
+var T_FOREACH_STATEMENT 	= exports.T_FOREACH_STATEMENT 		= iota();
+
 exports.module = module;
 function module(ports, stmts) {
-
+	return {
+		type 	: T_MODULE,
+		ports 	: ports,
+		body 	: stmts
+	};
 }
 
 exports.arrayLiteral = arrayLiteral;
 function arrayLiteral(items) {
-
+	return {
+		type 	: T_ARRAY_LITERAL,
+		items 	: items
+	};
 }
 
 exports.dictionaryLiteral = dictionaryLiteral;
 function dictionaryLiteral(pairs) {
-
+	return {
+		type 	: T_DICTIONARY_LITERAL,
+		pairs 	: pairs
+	};
 }
 
 exports.dictionaryPair = dictionaryPair;
 function dictionaryPair(key, value) {
-
+	return { key: key, value: value };
 }
 
 exports.globalObject = globalObject;
 function globalObject() {
-
+	return { type: T_GLOBAL_OBJECT };
 }
 
 exports.globalIdent = globalIdent;
 function globalIdent(name) {
-
+	return {
+		type 	: T_GLOBAL_IDENT,
+		name 	: name
+	};
 }
 
 exports.lambda = lambda;
 function lambda(args, body) {
-
+	return {
+		type 	: T_LAMBDA,
+		args 	: args,
+		body  	: body
+	};
 }
 
 exports.ident = ident;
 function ident(name) {
-
+	return {
+		type 	: T_IDENT,
+		name 	: name
+	};
 }
 
 exports.hexColor = hexColor;
@@ -50,27 +85,51 @@ function namedColor(name) {
 
 exports.whileStatement = whileStatement;
 function whileStatement(condition, body) {
-
+	return {
+		type 	: T_WHILE_STATEMENT,
+		cond 	: condition,
+		body 	: body
+	};
 }
 
 exports.loopWhileStatement = loopWhileStatement;
 function loopWhileStatement(condition, body) {
-
+	return {
+		type 	: T_LOOP_WHILE_STATEMENT,
+		cond 	: condition,
+		body 	: body
+	};
 }
 
 exports.loopStatement = loopStatement;
 function loopStatement(body) {
-
+	return {
+		type 	: T_LOOP_STATEMENT,
+		body 	: body
+	};
 }
 
 exports.forStatement = forStatement;
 function forStatement(subject, start, end, step, body) {
-
+	return {
+		type 	: T_FOR_STATEMENT,
+		subject : subject,
+		start 	: start,
+		end 	: end,
+		step 	: step,
+		body 	: body
+	};
 }
 
 exports.foreachStatement = foreachStatement;
 function foreachStatement(subject1, subject2, exp, body) {
-
+	return {
+		type 	: T_FOREACH_STATEMENT,
+		key 	: subject2 ? subject1 : null,
+		value 	: subject2 ? subject2 : subject1,
+		exp 	: exp,
+		body	: body
+	};
 }
 
 exports.ifStatement = ifStatement;
