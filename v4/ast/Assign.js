baa98324544c40deac7e7367ab11c618983f3e23
Jason Frame <jason@onehackoranother.com>
2015-06-27 02:40:55 +0100

    assignment

---
+++ v4/ast/Assign.js
@@ -0,0 +1,23 @@
+module.exports = Assign;
+
+var find = require('../env').find;
+
+function Assign(assignee, value) {
+	this.assignee = assignee;
+	this.value = value;
+}
+
+// TODO: doesn't support lvalues other than identifiers
+// needs work to support arrays, properties...
+Assign.prototype.evaluate = function(ctx, env, cont, err) {
+	var assignee = this.assignee;
+	return this.value.evaluate(ctx, env, function(value) {
+		var targetEnv = find(env, assignee.name);
+		if (!targetEnv) {
+			return ctx.thunk(err, new Error("undefined variable: " + assignee.name));
+		} else {
+			targetEnv[assignee.name] = value;
+			return ctx.thunk(cont, value);
+		}
+	}, err);
+}
